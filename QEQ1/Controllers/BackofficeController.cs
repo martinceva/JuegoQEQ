3bf773ad2c1bd450efea64820d8f5b46123d0106
Martin Ceva <cevaamartin@gmail.com>
2018-11-23 17:17:31 -0300

    Modificacion sobre BackOffice

--- QEQ1/Controllers/BackofficeController.cs
+++ QEQ1/Controllers/BackofficeController.cs
@@ -11,197 +11,326 @@ namespace QEQ1.Controllers
         // GET: Backoffice
         public ActionResult Menu()
         {
-            return View();
+            if (BD.isAdmin() == true)
+            {
+                return View();
+            }
+            else
+            {
+                return View("../QEQ/Login");
+            }
+
         }
-        
+
         public ActionResult ABMCategorias(Categorias ct)
         { //ver como asignar los personajes a las categorias
-            ViewBag.Categorias = BD.ListarCategorias();
-            return View();
+            //Llamar a una funcion que verifique que es administrador
+            if (BD.isAdmin() == true)
+            {
+                ViewBag.Categorias = BD.ListarCategorias();
+                return View();
+            }
+            else
+            {
+                return View("../QEQ/Login");
+            }
+
         }
+
         public ActionResult InsertarCategorias(string Accion)
         {
-            ViewBag.Accion = Accion;
-            return View();
+            if (BD.isAdmin() == true)
+            {
+                ViewBag.Accion = Accion;
+                return View();
+            }
+            else
+            {
+                return View("../QEQ/Login");
+            }
+
         }
 
         public ActionResult FormCategoria(string Accion, int Id)
         {
-            ViewBag.Categorias = BD.ListarCategorias();
-            ViewBag.Accion = Accion;
-            if (Accion == "Obtener")
+            if (BD.isAdmin() == true)
             {
-                Categorias ct = BD.ObtenerCategorias(Id);
-                return View("EdicionCategoria", ct);
+                ViewBag.Categorias = BD.ListarCategorias();
+                ViewBag.Accion = Accion;
+                if (Accion == "Obtener")
+                {
+                    if (!ModelState.IsValid)
+                    {
+                        return View("InsertarCategorias");
+                    }
+                    else
+                    {
+                        Categorias ct = BD.ObtenerCategorias(Id);
+                        return View("EdicionCategoria", ct);
+                    }
+                }
+                else
+                {
+                    if (Accion == "Eliminar")
+                    {
+                        BD.EliminarCategoria(Id);
+
+                        return View("ABMCategorias");
+                    }
+                }
+                return View("EdicionCategoria");
             }
             else
             {
-                if (Accion == "Eliminar")
-                {
-                    BD.EliminarCategoria(Id);
-                   
-                    return View("ABMCategorias");
-                }
+                return View("../QEQ/Login");
             }
-            return View("EdicionCategoria");
+
         }
         [HttpPost]
         public ActionResult EdicionCategoria(Categorias ct, string Accion)
         {
-            if (!ModelState.IsValid)
+            if (BD.isAdmin() == true)
             {
-                ViewBag.Categorias = BD.ListarCategorias();
-                return View("ABMCategorias");
-            }
-            else
-            {
-                if (Accion == "Obtener")
+                if (!ModelState.IsValid)
                 {
-                    BD.ModificarCategoria(ct);
                     ViewBag.Categorias = BD.ListarCategorias();
                     return View("ABMCategorias");
                 }
                 else
                 {
-                    if (Accion == "Insertar")
+                    if (Accion == "Obtener")
                     {
-                        BD.InsertarCategoria(ct);
-                        ViewBag.Categorias = BD.ListarCategorias();
-                        return View("ABMCategorias");
+                        if (!ModelState.IsValid)
+                        {
+                            ViewBag.Categorias = BD.ListarCategorias();
+                            return View("EdicionCategoria");
+                        }
+                        else
+                        {
+                            BD.ModificarCategoria(ct);
+                            ViewBag.Categorias = BD.ListarCategorias();
+                            return View("ABMCategorias");
+                        }
                     }
+                    else
+                    {
+                        if (Accion == "Insertar")
+                        {
+                            if (!ModelState.IsValid)
+                            {
+                                ViewBag.Categorias = BD.ListarCategorias();
+                                return View("InsertarCategorias");
+                            }
+                            else
+                            {
+                                BD.InsertarCategoria(ct);
+                                ViewBag.Categorias = BD.ListarCategorias();
+                                return View("ABMCategorias");
+                            }
+                        }
+                    }
+                    ViewBag.Categorias = BD.ListarCategorias();
+                    return View("ABMCategorias");
                 }
-                ViewBag.Categorias = BD.ListarCategorias();
-                return View("ABMCategorias");
+            }
+            else
+            {
+                return View("../QEQ/Login");
             }
 
         }
-        
-        
-         public ActionResult ABMPreguntas(Preguntas pr)
+
+        public ActionResult ABMPreguntas(Preguntas pr)
         {
-            ViewBag.Preguntas = BD.ListarPreguntas();
-            return View();
+            if (BD.isAdmin() == true)
+            {
+                ViewBag.Preguntas = BD.ListarPreguntas();
+                return View();
+            }
+            else
+            {
+                return View("../QEQ/Login");
+            }
+
         }
         public ActionResult InsertarPreguntas(string Accion)
         {
-            ViewBag.Accion = Accion;
-            return View();
+            if (BD.isAdmin() == true)
+            {
+                ViewBag.Accion = Accion;
+                return View();
+            }
+            else
+            {
+                return View("../QEQ/Login");
+            }
+
         }
 
         public ActionResult FormPregunta(string Accion, int Id)
         {
-            ViewBag.Preguntas = BD.ListarPreguntas();
-            ViewBag.Accion = Accion;
-            if (Accion == "Obtener")
+            if (BD.isAdmin() == true)
             {
-                Preguntas pr = BD.ObtenerPreguntas(Id);
-                return View("EdicionPreguntas", pr);
+                ViewBag.Preguntas = BD.ListarPreguntas();
+                ViewBag.Accion = Accion;
+                if (Accion == "Obtener")
+                {
+                    Preguntas pr = BD.ObtenerPreguntas(Id);
+                    return View("EdicionPreguntas", pr);
+                }
+                else
+                {
+                    if (Accion == "Eliminar")
+                    {
+                        BD.EliminarPregunta(Id);
+
+                        return View("ABMPreguntas");
+                    }
+                }
+                return View("EdicionPreguntas");
             }
             else
             {
-                if (Accion == "Eliminar")
-                {
-                    BD.EliminarPregunta(Id);
-                   
-                    return View("ABMPreguntas");
-                }
+                return View("../QEQ/Login");
             }
-            return View("EdicionPreguntas");
+
         }
         [HttpPost]
         public ActionResult EdicionPreguntas(Preguntas pr, string Accion)
         { //ver como asignar las preguntas a los personajes
-            if (!ModelState.IsValid)
+            if (BD.isAdmin() == true)
             {
-                ViewBag.Preguntas = BD.ListarPreguntas();
-                return View("ABMPreguntas");
-            }
-            else
-            {
-                if (Accion == "Obtener")
+                if (!ModelState.IsValid)
                 {
-                    BD.ModificarPregunta(pr);
                     ViewBag.Preguntas = BD.ListarPreguntas();
                     return View("ABMPreguntas");
                 }
                 else
                 {
-                    if (Accion == "Insertar")
+                    if (Accion == "Obtener")
                     {
-                        BD.InsertarPregunta(pr);
+                        BD.ModificarPregunta(pr);
                         ViewBag.Preguntas = BD.ListarPreguntas();
                         return View("ABMPreguntas");
                     }
+                    else
+                    {
+                        if (Accion == "Insertar")
+                        {
+                            BD.InsertarPregunta(pr);
+                            ViewBag.Preguntas = BD.ListarPreguntas();
+                            return View("ABMPreguntas");
+                        }
+                    }
+                    ViewBag.Preguntas = BD.ListarPreguntas();
+                    return View("ABMPreguntas");
                 }
-                ViewBag.Preguntas = BD.ListarPreguntas();
-                return View("ABMPreguntas");
+            }
+            else
+            {
+                return View("../QEQ/Login");
             }
 
         }
 
-        
-         public ActionResult ABMPersonajes(Personajes pe)
+
+
+
+        public ActionResult ABMPersonajes(Personajes pe)
         {
-            ViewBag.Personajes = BD.ListarPersonajes();
-            return View();
+            if (BD.isAdmin() == true)
+            {
+                ViewBag.Personajes = BD.ListarPersonajes();
+                return View();
+            }
+            else
+            {
+                return View("../QEQ/Login");
+            }
+
         }
         public ActionResult InsertarPersonajes(string Accion)
         {
-            ViewBag.Accion = Accion;
-            return View();
+            if (BD.isAdmin() == true)
+            {
+                ViewBag.Accion = Accion;
+                return View();
+            }
+            else
+            {
+                return View("../QEQ/Login");
+            }
+
         }
 
         public ActionResult FormPersonaje(string Accion, int Id)
         {
-            ViewBag.Preguntas = BD.ListarPersonajes();
-            ViewBag.Accion = Accion;
-            if (Accion == "Obtener")
+            if (BD.isAdmin() == true)
             {
-                Personajes pe = BD.ObtenerPersonajes(Id);
-                return View("EdicionPersonajes", pe);
+                ViewBag.Preguntas = BD.ListarPersonajes();
+                ViewBag.Accion = Accion;
+                if (Accion == "Obtener")
+                {
+                    Personajes pe = BD.ObtenerPersonajes(Id);
+                    return View("EdicionPersonajes", pe);
+                }
+                else
+                {
+                    if (Accion == "Eliminar")
+                    {
+                        BD.EliminarPersonaje(Id);
+
+                        return View("ABMPersonajes");
+                    }
+                }
+                return View("EdicionPersonajes");
             }
             else
             {
-                if (Accion == "Eliminar")
-                {
-                    BD.EliminarPersonaje(Id);
-                   
-                    return View("ABMPersonajes");
-                }
+                return View("../QEQ/Login");
             }
-            return View("EdicionPersonajes");
+
         }
         [HttpPost]
         public ActionResult EdicionPersonajes(Personajes pe, string Accion)
         {
-            if (!ModelState.IsValid)
+            if (BD.isAdmin() == true)
             {
-                ViewBag.Personajes = BD.ListarPersonajes();
-                return View("ABMPersonajes");
-            }
-            else
-            {
-                if (Accion == "Obtener")
+                if (!ModelState.IsValid)
                 {
-                    BD.ModificarPersonaje(pe);
                     ViewBag.Personajes = BD.ListarPersonajes();
                     return View("ABMPersonajes");
                 }
                 else
                 {
-                    if (Accion == "Insertar")
+                    if (Accion == "Obtener")
                     {
-                        BD.InsertarPersonajes(pe);
+                        BD.ModificarPersonaje(pe);
                         ViewBag.Personajes = BD.ListarPersonajes();
                         return View("ABMPersonajes");
                     }
+                    else
+                    {
+                        if (Accion == "Insertar")
+                        {
+                            BD.InsertarPersonajes(pe);
+                            ViewBag.Personajes = BD.ListarPersonajes();
+                            return View("ABMPersonajes");
+                        }
+                    }
+                    ViewBag.Personajes = BD.ListarPersonajes();
+                    return View("ABMPersonajes");
                 }
-                ViewBag.Personajes = BD.ListarPersonajes();
-                return View("ABMPersonajes");
             }
 
-        }
-         
+            else
+            {
+                return View("../QEQ/Login");
+            }
 
+            }
+
+        } 
+         
+       
     }
-}
--- QEQ1/Models/Personajes.cs
+++ QEQ1/Models/Personajes.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace QEQ1.Models
 {
@@ -9,6 +10,8 @@ namespace QEQ1.Models
     {
         private int _IDPersonaje;
         private string _NombrePersonaje;
+        
+
 
         public int IDPersonaje
         {
